ec2268123cfad6a48d4543917bf70e56dcb23a02
Andrew Schools <andrewschools@me.com>
2019-06-03 19:50:21 -0400

    Initial Commit

---
+++ product2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  product2
+//
+//  Created by Andrew Schools on 6/3/19.
+//  Copyright © 2019 Andrew Schools. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ product2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  product2
+//
+//  Created by Andrew Schools on 6/3/19.
+//  Copyright © 2019 Andrew Schools. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
